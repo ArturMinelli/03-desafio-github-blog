aff20298ebcf105a18a1cb7ac3d7f31d842b32a8
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-10-18 12:20:45 -0300

    creating router and application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles/global'
-import { Blog } from './pages/Blog'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Blog />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
     </ThemeProvider>
   )
 }
