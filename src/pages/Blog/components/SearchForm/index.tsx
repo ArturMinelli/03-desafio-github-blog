124c3c3c510de72a3da459b2329a837228331000
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-10-19 19:22:26 -0300

    applying react react hook form to search form

--- src/pages/Blog/components/SearchForm/index.tsx
+++ src/pages/Blog/components/SearchForm/index.tsx
@@ -1,17 +1,35 @@
 import { Input, SearchFormContainer } from "./styles";
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 interface SearchFormProps {
   issuesAmount: number;
 }
 
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>
+
 export function SearchForm({ issuesAmount }: SearchFormProps) {
+  const { register, handleSubmit } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  function handleSearchPosts(data: SearchFormInputs) {
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <div>
         Publicações
         <span> {issuesAmount && `${issuesAmount} publicações`} </span>
       </div>
       <Input
+        {...register('query')}
         placeholder="Buscar conteúdo"
       />
     </SearchFormContainer>
